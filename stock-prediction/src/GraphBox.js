38cce087288b6dc0a229b61134f61782806b64ea
nishnareddy1 <aletinishna0723@gmail.com>
2020-11-29 13:21:45 -0800

    adjusting padding in graphbox- right corner

--- stock-prediction/src/GraphBox.js
+++ stock-prediction/src/GraphBox.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 import { Line } from "react-chartjs-2";
-// import Plot from 'react-plotly.js';
 import './GraphBox.css';
 import moment from 'moment';
-// import { useAlert } from 'react-alert';
 class GraphBox extends React.Component{
     constructor(props){
         super(props);
@@ -13,42 +11,33 @@ class GraphBox extends React.Component{
         };}
     componentDidUpdate(pP){
         if(pP.currentStockCode != this.props.currentStockCode){
-            // console.log(this.props.currentStockCode);
-            // console.log(this.props.alter_Graph);
             if (this.props.alter_Graph===false){
               var startDate = Math.round(new Date().getTime() / 1000);
               var endDate = startDate - (72 * 3600);
-              
             }
             else{
               var startDate = this.props.dates.endDate;
               var endDate = this.props.dates.startDate;
             }
             const pointerToThis = this;
-          axios.get('https://finnhub.io/api/v1/stock/candle',{
-              
+            axios.get('https://finnhub.io/api/v1/stock/candle',{              
             params:{
                 symbol: this.props.currentStockCode,
                 resolution: 5,
                 from: endDate,
                 to: startDate,
                 token: 'bu5pnnf48v6qku34c7vg'
-
             }})
             .then((response) => {
               console.log(response);
-              // console.log({ JSON.stringify(new Date(response.data.t * 1000)).split('T')[0].replace('"', '') });
-              // console.log(response);
               var unix_time = response.data.t;
               var i;
               var x=[];
-              // console.log(unix_time);
                 for (i = 0; i < unix_time.length; i++) {
                   var time = moment.unix(unix_time[i]).format("YYYY-MM-DD HH:mm");
                   x.push(time)
                 }
               var y = response.data.c;
-              // console.log(typeof(y[0]));
               var lowOrHighColor = y[0] < y[y.length - 1] 
                 ? '#81b737' : '#d54f4f';
               this.setState({
@@ -79,12 +68,10 @@ class GraphBox extends React.Component{
                     ],
                   },
                 });
-              
-  
             })
             .catch(error => {
-    console.log(error.response)
-});
+              console.log(error.response)
+            });
         }
         else if(pP.dates!=this.props.dates){
           if (this.props.alter_Graph===false){
@@ -106,12 +93,13 @@ class GraphBox extends React.Component{
               to: startDate,
               token: 'bu5pnnf48v6qku34c7vg'
 
-          }})
+          }
+        }
+        )
           .then((response) => {
             console.log(response.data);
             if (response.data.s=="no_data")
             {
-              // const alert = useAlert();
               alert('No Data Currently Available. Markets are closed during weekends and public holidays. Please filter by previous date.');
             }
             else{
@@ -126,13 +114,15 @@ class GraphBox extends React.Component{
               var text='between '+String(x[0]).slice(0,10)+' to '+String(x[x.length-1].slice(0,10))
               var y = response.data.c;
               var lowOrHighColor = y[0] < y[y.length - 1] 
-                ? '#81b737' : '#d54f4f';
+                ? 
+                '#81b737' : '#d54f4f';
               this.setState({
                 text: text,
                 symbol: this.props.currentStockCode,
                 Data: {
                   labels: x,
-                  datasets: [
+                  datasets: 
+                  [
                     {
                       label: "Stock Price",
                       data: y,
@@ -154,12 +144,15 @@ class GraphBox extends React.Component{
                     },
                   ],
                 },
-              });
+              }
+              );
             }
-          })
+          }
+          )
           .catch(error => {
             console.log(error.response)
-            });
+            }
+            );
         }
 
       }
